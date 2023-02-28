58db89d876f1da2174606947f1af91fd0eba01a8
sivakumar0703 <ssivakumar358@gmail.com>
2023-02-28 12:33:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+var display = document.querySelector('#heading');
+display.style.textAlign = 'center' ;
+display.style.margin = '300px' ;
+display.style.fontSize = '50px' ;
+
+
+
+setTimeout(() => {
+     display.innerText = '10' ;
+
+     setTimeout(() => {
+        display.innerText = '9' ;
+
+        setTimeout(() => {
+            display.innerText = '8' ;
+
+            setTimeout(() => {
+                display.innerText = '7' ;
+
+                setTimeout(() => {
+                    display.innerText = '6' ;
+
+                    setTimeout(() => {
+                        display.innerText = '5' ;
+
+                        setTimeout(() => {
+                            display.innerText = '4' ;
+
+                            setTimeout(() => {
+                                display.innerText = '3' ;
+
+                                setTimeout(() => {
+                                    display.innerText = '2' ;
+
+                                    setTimeout(() => {
+                                        display.innerText = '1' ;
+
+                                        setTimeout(() => {
+                                            display.innerText = 'Happy Independence Day' ;
+                                        },1000)
+
+                                    },1000)
+
+                                },1000)
+
+                            },1000)
+
+                        },1000)
+
+                    },1000)
+
+                },1000)
+
+            },1000)
+
+        },1000)
+
+     },1000)
+
+},1000)
